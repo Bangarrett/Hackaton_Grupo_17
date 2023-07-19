80a1649f107beca36bf99e69b894cf402d4633d7
Cynthia <cyn2993@gmail.com>
2023-07-19 14:42:50 +0200

    Changes navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import './App.css'
 import TrendingTracks from './components/Carrousel'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
+      <Navbar />
       <TrendingTracks />
     </>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <>
+    <nav>
+        <div class="logo"><img src="src/assets/logo.png" alt="Image of the logo"></img></div>
+        <div class="slogan">Unlock the Beat: Your One-Stop Song Shop</div>
+        <div class="search"><input type="search" placeholder="Search"></input></div>
+    </nav>
+    </>
+    )
+}
+
+export default Navbar;
