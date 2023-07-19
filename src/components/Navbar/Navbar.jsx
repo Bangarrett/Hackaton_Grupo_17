ba7d52c6a618e3227e55809dba2a124911150ffb
Cynthia <cyn2993@gmail.com>
2023-07-19 19:04:06 +0200

    Add navbar in mobile and desktop view

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
         <>
     <nav>
         <div class="nav__logo"><img src="src/assets/logo.png" alt="Image of the logo"></img></div>
+        <div class="nav__slogan">Unlock the Beat: Your One-Stop Song Shop</div>
         <div class="nav__search"><input type="search" placeholder="Search"></input><a href="#"><img class="nav__search--logo" src="src\assets\searchlogo.png"></img></a></div>
     </nav>
     </>
