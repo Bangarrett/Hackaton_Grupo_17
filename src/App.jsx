543d46eb0af1d3484b473e0cd74c4662d0247406
helenaponted <helenaponte@hotmail.es>
2023-07-19 17:47:18 +0100

    add card2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import TrendingTracks from './components/Carrousel'
 import Merchandising from './components/card-item/CardItem'
+import Merchandising2 from './components/card-item/CardItem2'
 function App() {
   return (
     <>
       <TrendingTracks />
+      <div id="card-item">
       <Merchandising />
+      <Merchandising2/>
+      </div>
     </>
   )
 }
