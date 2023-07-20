17301ea514c2fc15e317ccd75e6c02a374733925
helenaponted <helenaponte@hotmail.es>
2023-07-20 16:18:04 +0100

    add style

--- src/components/notification/Notification.jsx
+++ src/components/notification/Notification.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-  
+import './Notification.css';
+
   function Notification(){
     const notify = () => toast("all products at 30% off this month with the code WELCOME ");
 
     return (
-      <div>
-        <button className="button" onClick={notify}>Discount</button>
+      <div >
+        <button className='notification' onClick={notify}>Discount</button>
         <ToastContainer />
       </div>
     );
