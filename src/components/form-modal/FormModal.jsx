b7a3c09410338eb155cb42f0bc51c1544311bee0
Cynthia <cyn2993@gmail.com>
2023-07-20 20:31:23 +0200

    Change form language

--- src/components/form-modal/FormModal.jsx
+++ src/components/form-modal/FormModal.jsx
@@ -20,23 +20,23 @@ const FormModal = () => {
   return (
     <div>
       <div className="button-container">
-      <button className="open-button contact-button" onClick={handleOpenModal}>Contáctanos</button>
+      <button className="open-button contact-button" onClick={handleOpenModal}>CONTACT US</button>
     </div>
       {/* Modal */}
       {modalOpen && (
         <div className="form-modal">
           <div className="form-container">
-            <h2>¿Qué necesitas? 🙂</h2>
+            <h2>What do you need? 🙂</h2>
             <div className="form-content">
-            <label className="name" htmlFor="name">Nombre:</label>
+            <label className="name" htmlFor="name">Name:</label>
             <input type="text" id="name" name="name" required />
 
             <label className="email" htmlFor="email">Email:</label>
             <input type="email" id="email" name="email" required />
             </div>
             <div className="buttons">
-            <button className="open-button send-button" onClick={handleSubmit}>Enviar</button>
-            <button className="open-button close-button" onClick={handleCloseModal}>Cancelar</button>
+            <button className="open-button send-button" onClick={handleSubmit}>Submit</button>
+            <button className="open-button close-button" onClick={handleCloseModal}>Cancel</button>
             </div>
           </div>
         </div>
