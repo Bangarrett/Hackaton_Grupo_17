f2a3fe4f1858b7b74a9329149b5ce273c039d737
Erikalinares <erikalinares421@gmail.com>
2023-07-20 10:54:02 +0200

    footer terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import TrendingTracks from './components/Carrousel'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <>
+
       <TrendingTracks />
+      <Footer />
     </>
   )
 }
